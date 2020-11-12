0c7e0dc62ae28ef2571118f61b56fa6f093b3aab
MDC <markdcross@gmail.com>
2020-11-12 17:00:05 -0500

    completed views, created script.js for event listeners

--- server.js
+++ server.js
@@ -19,7 +19,9 @@ app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
 // Import routes and give the server access to them
-//TODO Import routes and give the server access to them.
+const routes = require('./controllers/burgers_controller');
+
+app.use(routes);
 
 // Start listener
 app.listen(PORT, function () {

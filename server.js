bc3d9836ce65cbdefb50b158062ba1f9d9262841
MDC <markdcross@gmail.com>
2020-11-12 11:15:17 -0500

    updated server.js

--- server.js
+++ server.js
@@ -1,1 +1,22 @@
 const express = require('express');
+
+const PORT = process.env.PORT || 4848;
+
+const app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static('public'));
+
+// Parse request body as JSON
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Set Handlebars
+const exphbs = require('express-handlebars');
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set("view engine", "handlebars");   
+
+// Import routes and give the server access to them
+//TODO Import routes and give the server access to them.

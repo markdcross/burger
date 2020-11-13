a50790e3215349dd71bb212d842e1ffdd95c03a2
MDC <markdcross@gmail.com>
2020-11-13 11:59:03 -0500

    fixed scope issues and app is fully functional on local

--- config/orm.js
+++ config/orm.js
@@ -41,7 +41,7 @@ function objToSql(ob) {
 
 const orm = {
     selectAll: function (tableInput, cb) {
-        const queryString = `SELECT * FROM ${tableInput};`;
+        let queryString = `SELECT * FROM ${tableInput};`;
         connection.query(queryString, function (err, result) {
             if (err) {
                 throw err;
@@ -50,9 +50,8 @@ const orm = {
         });
     },
 
-    //TODO Refactor to template literals?
     insertOne: function (table, cols, vals, cb) {
-        const queryString = 'INSERT INTO ' + table;
+        let queryString = 'INSERT INTO ' + table;
 
         queryString += ' (';
         queryString += cols.toString();
@@ -72,9 +71,8 @@ const orm = {
         });
     },
 
-    //TODO Refactor to template literals?
     updateOne: function (table, objColVals, condition, cb) {
-        const queryString = 'UPDATE ' + table;
+        let queryString = 'UPDATE ' + table;
 
         queryString += ' SET ';
         queryString += objToSql(objColVals);
--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -26,7 +26,7 @@ router.post('/api/burgers', function (req, res) {
 });
 
 router.put('/api/burgers/:id', function (req, res) {
-    const condition = `id = ${req.params.id}`;
+    let condition = `id = ${req.params.id}`;
 
     console.log('condition', condition);
 
@@ -36,7 +36,7 @@ router.put('/api/burgers/:id', function (req, res) {
         },
         condition,
         function (results) {
-            if (result.changedRows == 0) {
+            if (results.changedRows == 0) {
                 // If no rows were changed, then the ID must not exist, so 404
                 return res.status(404).end();
             } else {
